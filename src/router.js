9cbeb317044c2d9f92941b61ad82b369bdfb57cb
Shandaram <anohinaksenia1701@gmail.com>
2024-03-08 02:20:46 +0000

    final commit Thursday

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
+    path: "/rentersnet",
     component: () => import("./views/HomePage.vue"),
   },
   {
